01d5039c8f53009bbc8eaf09dcb18e0420a3666a
= <wz961681117@gmail.com>
2016-04-30 00:56:58 +0800

    init

--- app/src/main/java/com/roselism/callpp/activity/LoginActivity.java
+++ app/src/main/java/com/roselism/callpp/activity/LoginActivity.java
@@ -1,14 +1,15 @@
-package com.roselism.callpp;
+package com.roselism.callpp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.roselism.callpp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
-
--- app/src/main/java/com/roselism/callpp/activity/MainActivity.java
+++ app/src/main/java/com/roselism/callpp/activity/MainActivity.java
@@ -1,13 +1,16 @@
-package com.roselism.callpp;
+package com.roselism.callpp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.roselism.callpp.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
+
