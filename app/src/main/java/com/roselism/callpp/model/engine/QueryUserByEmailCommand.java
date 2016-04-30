b80bdcc30b0ac5713f557fff3e46a6bec0a8a999
= <wz961681117@gmail.com>
2016-04-30 13:48:20 +0800

    框架在LoginActivity中的初体验

--- app/src/main/java/com/roselism/callpp/model/engine/QueryUserByEmailCommand.java
+++ app/src/main/java/com/roselism/callpp/model/engine/QueryUserByEmailCommand.java
@@ -1,7 +1,5 @@
 package com.roselism.callpp.model.engine;
 
-import com.roselism.callpp.model.engine.stragegy.OnOperatListener;
-
 /**
  * 具体策略
  * Created by simon on 2016/4/30.
@@ -9,13 +7,35 @@ import com.roselism.callpp.model.engine.stragegy.OnOperatListener;
 public class QueryUserByEmailCommand implements Command {
 
     QueryUserReceiver receiver;
+    String email;
 
     public QueryUserByEmailCommand(QueryUserReceiver receiver) {
         this.receiver = receiver;
     }
 
+    /**
+     * @param receiver 接收者
+     * @param email    要被查询的邮箱
+     */
+    public QueryUserByEmailCommand(QueryUserReceiver receiver, String email) {
+        this.receiver = receiver;
+        this.email = email;
+    }
+
+    /**
+     * 设置邮箱
+     *
+     * @param email 邮箱地址
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public void excute() {
-        receiver.queryUserByEmail();
+        if (email == null)
+            throw new NullPointerException("邮箱地址不能为null");
+
+        receiver.queryUserByEmail(email);
     }
 }
