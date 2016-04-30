3aadca35253650fbd9a70ecacd4d4f6894b4523f
lairuguo <948646232@qq.com>
2016-04-30 13:31:58 +0800

    主界面Toolbar功能实现

---
+++ app/src/main/java/com/roselism/callpp/fragment/UserFragment.java
@@ -0,0 +1,13 @@
+package com.roselism.callpp.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @创建者 lai
+ * @创建时间 2016/4/30
+ * @packageName com.roselism.callpp.activity
+ * @更新时间 2016/4/30 13:23
+ * @描述 TODO
+ */
+public class UserFragment extends Fragment {
+}
