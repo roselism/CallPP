87ff8e53e7b0b1e6a049faf9a9c31aacaefcbdc4
simon <hero21c@outlook.com>
2016-05-03 22:53:17 +0800

    清洁CallppApplication.java

--- app/src/main/java/com/roselism/callpp/CallppApplication.java
+++ app/src/main/java/com/roselism/callpp/CallppApplication.java
@@ -9,18 +9,12 @@ import com.roselism.callpp.util.LogUtil;
 /**
  * Created by simon on 2016/4/30.
  */
-public class CallPPApplication extends Application{
+public class CallppApplication extends Application {
 
     private static Handler sMainHandler = new Handler();
 
     private static Context sContext;// Application的上下文
     private static int sMainThreadId;// 主线程Handler
-
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//    }
-
 
     /**
      * 获取context
@@ -43,17 +37,8 @@ public class CallPPApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-
-        LogUtil.i( "onCreate");
-
         sContext = getApplicationContext();
         sMainThreadId = android.os.Process.myTid();
-
-//        LogUtil.setIsDebug(true); // 开启debug模式
-
-//        BmobIniter bmobIniter = new BmobIniter(sContext); // 初始化bmob全局变量
-//        bmobIniter.initBmob();
-
     }
 
     public static Handler getMainHandler() {
